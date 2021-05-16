16192b2e51fa0128121d25acf892489a5a3fe7b6
Amirhossein <amirhosseinkz98@gmail.com>
2021-05-17 00:36:19 +0430

    view

--- lib/view/widgets/custom_listview.dart
+++ lib/view/widgets/custom_listview.dart
@@ -7,62 +7,66 @@ import 'package:flutter_appq/view/style/textstyle.dart';
 import 'package:provider/provider.dart';
 
 Widget customListView(BuildContext context) {
-
-
   final state = Provider.of<CountryControllerProvider>(context, listen: true);
 
   Size size = MediaQuery.of(context).size;
 
   state.fetchdata();
 
-  return Stack(children: [
-    Container(
-      child: state.countryList.length > 0
-          ? ListView.builder(
-              itemBuilder: (context, index) {
-                return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                        decoration: BoxDecoration(
-                            color: customListViewContainerColor,
-                            borderRadius: BorderRadius.circular(10)),
-                        height: size.height * 0.18,
-                        width: size.width * 0.8,
-                        child: Card(
-                          elevation: 5,
-                          child: Center(
-                            child: ListTile(
-                              title: AutoSizeText(
-                                state.countryList[index].name,
-                                style: listViewTitleStyle,
-                              ),
-                              leading: Text(
-                                state.countryList[index].id.toString() + '-',
-                                style: listViewLeadStyle,
-                              ),
-                              subtitle: Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(vertical: 8),
-                                child: AutoSizeText(
-                                  'Country_id:' +
-                                      state.countryList[index].countryId
-                                          .toString(),
-                                  style: listViewSubStyle,
-                                ),
-                              ),
-                            ),
-                          ),
-                        )));
-              },
-              itemCount: state.countryList.length,
-            )
-          : Center(
-              child: CircularProgressIndicator(
-                valueColor: new AlwaysStoppedAnimation<Color>(
-                  Colors.blue,
-                ),
-              ),
-            ),
-    ),
-  ]);
+  return state.isLoading || !state.isData ?
+      Center(
+       child: CircularProgressIndicator(),
+     ): state.countryList.isEmpty ?
+     Center(
+       child: Column(
+         mainAxisAlignment: MainAxisAlignment.center,
+         children: [
+           Text(
+             'لطفا اینترنت خود را روشن کنید',
+             style: listViewTitleStyle,
+           ),
+           TextButton(
+               onPressed: () {
+                 state.fetchdata();
+               },
+               child: Text('تلاش مجدد'))
+         ],
+       ),
+     ):
+   ListView.builder(
+     itemBuilder: (context, index) {
+   return Padding(
+       padding: const EdgeInsets.all(8.0),
+       child: Container(
+           decoration: BoxDecoration(
+               color: customListViewContainerColor,
+               borderRadius: BorderRadius.circular(10)),
+           height: size.height * 0.18,
+           width: size.width * 0.8,
+           child: Card(
+             elevation: 5,
+             child: Center(
+               child: ListTile(
+                 title: AutoSizeText(
+                   state.countryList[index].name,
+                   style: listViewTitleStyle,
+                 ),
+                 leading: Text(
+                   state.countryList[index].id.toString() + '-',
+                   style: listViewLeadStyle,
+                 ),
+                 subtitle: Padding(
+                   padding: const EdgeInsets.symmetric(vertical: 8),
+                   child: AutoSizeText(
+                     'Country_id:' +
+                         state.countryList[index].countryId.toString(),
+                     style: listViewSubStyle,
+                   ),
+                 ),
+               ),
+             ),
+           )));
+     },
+     itemCount: state.countryList.length,
+   );
 }
